41decf7c7c17790f20a3017a08fcf614a51ab67b
jh-02 <jaehoonjeong@kakao.com>
2024-08-25 11:01:14 +0900

    anaconda environment test

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,15 @@
+a = 5
+
+
 print('hello')
+print('hello')
+print('hello')
+print('hello')
+# print('hello')
+
+#shift + 위아래화살표 (드래그)
+#alt + 위아래화살표 (코드이동)
+#alt + shift + 위아래화살표 (코드복사)
+#ctrl + x (코드삭제)
+# ctrl + / (주석처리/해제)
 #https
-#ssh
-#After device format
